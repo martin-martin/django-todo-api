ba8503dc3e08302c0f1235d82caf181f085e5e67
martin-martin <breuss.martin@gmail.com>
2018-09-30 17:43:34 +0200

    add project-wide URL routing

--- auth_api/auth_api/urls.py
+++ auth_api/auth_api/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from . import views  # the . is a shortcut for current App (here 'auth_api')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', include_docs_urls(title='Todo API',
+                                    description='RESTful API for Todo items')),
+    path('', views.api_root),
+    path('', include(users.urls), namespace='users'),
+    path('', include(todos.urls), namespace='todos'),
 ]
