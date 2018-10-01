d87c753eae70efc2052bb83d17d17f2626a76e98
martin-martin <breuss.martin@gmail.com>
2018-10-01 09:16:02 +0200

    add class-based views

--- auth_api/users/views.py
+++ auth_api/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from .models import User
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        serializer_class = serializer.save(user=self.request.user)
+
+
+class UserDetail(generics.ListRetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.all().filter(username=self.request.user)
