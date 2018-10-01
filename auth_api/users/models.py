ae1b847db4defa11b111b977a30f34a01ccb46b1
martin-martin <breuss.martin@gmail.com>
2018-10-01 10:07:00 +0200

    add models

--- auth_api/users/models.py
+++ auth_api/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+'''
+We use the same AbstractUser that the default Django User model uses,
+but defining our own User model gives us the ability to easily edit it
+later on while still retaining the same features as the default Django User.
+'''
+
+
+class User(AbstractUser):
+    pass
